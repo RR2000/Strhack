865d8bc5b9a619d8f3e23acd86c47c07402dc0e3
RR2000 <r2000raoul@gmail.com>
2023-06-19 00:04:47 +0200

    +Added speed and MVVM to course viewer

--- app/src/main/java/com/rondinella/strhack/ui/viewer/CourseViewerActivity.kt
+++ app/src/main/java/com/rondinella/strhack/ui/viewer/CourseViewerActivity.kt
@@ -1,25 +1,26 @@
 package com.rondinella.strhack.ui.viewer
 
 import android.animation.ArgbEvaluator
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Paint
-import android.net.Uri
+import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ProgressBar
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceManager
 import com.rondinella.strhack.R
-import com.rondinella.strhack.ui.editor.CourseEditorActivity
 import com.rondinella.strhack.databinding.ActivityCourseViewerBinding
 import com.rondinella.strhack.tracker.AdvancedGeoPoint
 import com.rondinella.strhack.tracker.Course
+import com.rondinella.strhack.ui.editor.CourseEditorActivity
 import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
 import org.osmdroid.config.Configuration
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Polyline
@@ -29,289 +30,146 @@ import java.io.*
 
 class CourseViewerActivity : AppCompatActivity() {
 
-    private var _binding: ActivityCourseViewerBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: ActivityCourseViewerBinding
+    private val courseViewModel: CourseViewerViewModel by viewModels()
 
-    fun getFileFromPath(file: File): ByteArray {
-        val size = file.length().toInt()
-        val bytes = ByteArray(size)
-        try {
-            val buf = BufferedInputStream(FileInputStream(file))
-            buf.read(bytes, 0, bytes.size)
-            buf.close()
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return bytes
-    }
-
-
-    private fun handleReceiveGpx(intent: Intent): String {
-        val bytes: ByteArray
-
-        val gpxUri: Uri = intent.data!!
-        val file = File(cacheDir, "gpx")
-        val inputStream: InputStream? = contentResolver.openInputStream(gpxUri)
-        try {
-            FileOutputStream(file).use { output ->
-                val buffer = ByteArray(4 * 1024) // or other buffer size
-                var read: Int
-                while (inputStream?.read(buffer).also { read = it!! } != -1) {
-                    output.write(buffer, 0, read)
-                }
-                output.flush()
-            }
-        } finally {
-            inputStream?.close()
-            bytes = getFileFromPath(file)
-        }
-
-        return String(bytes)
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 42 && resultCode == 0) finish()
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        _binding = ActivityCourseViewerBinding.inflate(layoutInflater)
-        setTheme(R.style.AppTheme_NoActionBar)
-        setContentView(R.layout.activity_course_viewer)
-        binding.toolbarCourseViewer.setTitleTextColor(Color.WHITE)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCourseViewerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setTheme(R.style.AppTheme_NoActionBar)
+        setupToolbar()
+        setupCourseLiveDataObserver()
+        setupSegmentsLiveDataObserver()
+        setupButtons()
+        loadCourseOnStart()
     }
 
-    override fun onResume() {
-        super.onResume()
+    private fun setupToolbar() {
+        binding.toolbarCourseViewer.apply {
+            setTitleTextColor(Color.WHITE)
+            setNavigationIcon(R.drawable.ic_arrow_back)
+            setNavigationOnClickListener { onBackPressed() }
 
-
-        Configuration.getInstance().load(baseContext, PreferenceManager.getDefaultSharedPreferences(baseContext))
-
-        lateinit var course: Course
-        lateinit var limitedGeoPoints: ArrayList<AdvancedGeoPoint>
-
-        if (intent.action == Intent.ACTION_VIEW) {
-            course = Course(handleReceiveGpx(intent))
-            title = course.geoPoints()[0].date.toString()
-        }
-
-        CoroutineScope(Main).launch {
-            if (intent.action == Intent.ACTION_VIEW) {
-                course = Course(handleReceiveGpx(intent))
-                title = course.geoPoints()[0].date.toString()
-            } else {
-                course = Course(File(applicationContext.filesDir.absolutePath + "/tracks/" + intent.getStringExtra("filename")))
-                binding.toolbarCourseViewer.title = intent.getStringExtra("title")
-            }
-            delay(1000)//TODO implement MVC
-            binding.idMapGpxViewer.setBuiltInZoomControls(false)
-            binding.idMapGpxViewer.setMultiTouchControls(true)
-            val overlayRotation = RotationGestureOverlay(binding.idMapGpxViewer).apply { isEnabled = true }
-            binding.idMapGpxViewer.overlays.add(overlayRotation)
-
-            //Get controller of the map
-            binding.idMapGpxViewer.controller.setZoom(15.0)
-            binding.idMapGpxViewer.controller.animateTo(course.centralPoint())
-            binding.idMapGpxViewer.zoomToBoundingBox(course.boundingBox(), true)
-
-            binding.idMapGpxViewer.setOnTouchListener { v, event ->
-                val viewPager = binding.scrollviewCourseViewer
-                when (event.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        viewPager.requestDisallowInterceptTouchEvent(true)
-                    }
-                    MotionEvent.ACTION_UP -> {
-                        viewPager.requestDisallowInterceptTouchEvent(false)
-                        v.performClick()
-                    }
-                    MotionEvent.ACTION_CANCEL -> {
-                        viewPager.requestDisallowInterceptTouchEvent(false)
-                    }
+            setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.button_remove_course -> confirmDeleteCourse()
+                    R.id.button_edit_course -> openCourseEditor()
                 }
-                false
+                true
             }
+        }
+    }
 
-            withContext(IO) {
-                limitedGeoPoints = course.getPointEveryMeters(0.00001)
+    private fun confirmDeleteCourse() {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.delete_title))
+            .setMessage(getString(R.string.delete_message))
+            .setPositiveButton(getString(R.string.delete)) { _, _ ->
+                val courseFile = File(applicationContext.filesDir.absolutePath + "/tracks/" + intent.getStringExtra("filename"))
+                if (courseFile.exists()) courseFile.delete()
+                finish()
             }
-            //Log.d("DEBUG", "Central Point: ${course.centralPoint()}")
-            //Log.d("DEBUG", "Bounding Box: ${course.boundingBox()}")
+            .setNegativeButton(getString(R.string.cancel), null)
+            .show()
+    }
 
-            binding.buttonBlankMap.performClick()
+    private fun openCourseEditor() {
+        val intentEditor = Intent(this, CourseEditorActivity::class.java)
+        intentEditor.putExtra("filename", intent.getStringExtra("filename"))
+        startActivityForResult(intentEditor, 42)
+    }
 
-            binding.textDistance.text = course.getDistance().toString() + " km"
-            binding.averageSpeed.text = course.getAverageSpeed().toString() + " km/h"
-            binding.totalTime.text = course.getTotalTime()
-            binding.elevationGain.text = course.getTotalElevationGain().toString() + " m"
-
+    private fun setupCourseLiveDataObserver() {
+        courseViewModel.course.observe(this) { course ->
+            setupCourseDetails(course)
+            courseViewModel.drawBlankMap()
         }
+    }
 
-        binding.toolbarCourseViewer.setNavigationOnClickListener {
-            onBackPressed()
+    private fun setupSegmentsLiveDataObserver() {
+        courseViewModel.segments.observe(this) { segments ->
+            loadSegmentsToMap(segments)
         }
+    }
 
-        binding.toolbarCourseViewer.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.button_remove_course -> {
-                    AlertDialog.Builder(this).setTitle(getString(R.string.delete_title)).setMessage(getString(R.string.delete_message))
-                        .setPositiveButton(getString(R.string.delete)) { dialogInterface, i ->
-                            File(applicationContext.filesDir.absolutePath + "/tracks/" + intent.getStringExtra("filename")).delete()
-                            finish()//startActivity(Intent(this, MainActivity::class.java))
-                        }.setNegativeButton(getString(R.string.cancel), null).show()
-                }
-                R.id.button_edit_course -> {
-                    val intentEditor = Intent(this, CourseEditorActivity::class.java)
-                    intentEditor.putExtra("filename", intent.getStringExtra("filename"))
-                    startActivityForResult(intentEditor, 42)
-                }
-            }
-            true
-        }
+    private fun setupButtons() {
         binding.buttonBlankMap.setOnClickListener {
-            CoroutineScope(Main).launch {
-                drawBlankMap(limitedGeoPoints, binding.idMapGpxViewer, binding.loadingCourseCircle)
-            }
+            courseViewModel.drawBlankMap()
         }
-
         binding.buttonSlope.setOnClickListener {
-            CoroutineScope(Main).launch {
-                drawSlopeMap(limitedGeoPoints, binding.idMapGpxViewer, binding.loadingCourseCircle)
-            }
+            courseViewModel.drawSlopeMap()
         }
-
         binding.buttonAltitudeDifference.setOnClickListener {
-            CoroutineScope(Main).launch {
-                drawAltitudeDifferenceMap(course, limitedGeoPoints, binding.idMapGpxViewer, binding.loadingCourseCircle)
-            }
+            courseViewModel.drawAltitudeDifferenceMap()
         }
-
-    }
-
-    private suspend fun drawBlankMap(geoPoints: ArrayList<AdvancedGeoPoint>, map: MapView, loadingCourseCircle: ProgressBar) {
-        Log.d("DEBUG", "drawBlankMap started")
-        map.overlayManager.removeAll(map.overlays)
-
-        map.visibility = View.INVISIBLE
-        loadingCourseCircle.visibility = View.VISIBLE
-
-        val segments = withContext(Dispatchers.IO) {
-            geoPoints.windowed(2, 1).map { (start, end) ->
-                val seg = Polyline()
-                seg.addPoint(start)
-                seg.addPoint(end)
-
-                seg.outlinePaint.strokeCap = Paint.Cap.ROUND
-                seg
-            }
-        }
-        withContext(Dispatchers.Main) {
-            map.overlayManager.addAll(segments)
-            map.visibility = View.VISIBLE
-            loadingCourseCircle.visibility = View.INVISIBLE
+        binding.buttonSpeed.setOnClickListener {
+            courseViewModel.drawSpeedMap()
         }
     }
 
+    private fun loadCourseOnStart() {
+        val courseFile = File(applicationContext.filesDir.absolutePath + "/tracks/" + intent.getStringExtra("filename"))
+        courseViewModel.loadCourse(courseFile)
+    }
 
-    private suspend fun drawAltitudeDifferenceMap(course: Course, geoPoints: ArrayList<AdvancedGeoPoint>, map: MapView, loadingCourseCircle: ProgressBar) {
-        Log.d("DEBUG", "drawAltitudeDifferenceMap started")
-        map.overlayManager.removeAll(map.overlays)
+    @SuppressLint("SetTextI18n")
+    private fun setupCourseDetails(course: Course) {
+        binding.toolbarCourseViewer.title = intent.getStringExtra("title")
+        setupMapView(course)
+        binding.textDistance.text = course.getDistance().toString() + " km"
+        binding.averageSpeed.text = course.getAverageSpeed().toString() + " km/h"
+        binding.totalTime.text = course.getTotalTime()
+        binding.elevationGain.text = course.getTotalElevationGain().toString() + " m"
+    }
 
-        val maxAltitude = course.geoPoints().maxBy { it.altitude }.altitude
-        val minAltitude = course.geoPoints().minBy { it.altitude }.altitude
+    private fun setupMapView(course: Course) {
+        binding.idMapGpxViewer.apply {
+            setBuiltInZoomControls(false)
+            setMultiTouchControls(true)
+            val overlayRotation = RotationGestureOverlay(this).apply { isEnabled = true }
+            overlays.add(overlayRotation)
 
+            // Get controller of the map
+            controller.setZoom(15.0)
+            controller.animateTo(course.centralPoint())
+            zoomToBoundingBox(course.boundingBox(), true)
 
-        map.visibility = View.INVISIBLE
-        loadingCourseCircle.visibility = View.VISIBLE
-
-        // Create a color interpolator for smoother transitions
-        val colorInterpolator = ArgbEvaluator()
-
-        val segments = withContext(Dispatchers.IO) {
-            geoPoints.windowed(2, 1).map { (start, end) ->
-                val seg = Polyline()
-                seg.addPoint(start)
-                seg.addPoint(end)
-
-                var altitudeRatio: Float = ((end.altitude - minAltitude) / (maxAltitude - minAltitude)).toFloat()
-
-                // Clamp ratio to 0..1 range just in case
-                altitudeRatio = altitudeRatio.coerceIn(0f, 1f)
-                val color = colorInterpolator.evaluate(altitudeRatio, Color.GREEN, Color.BLACK) as Int
-
-                seg.outlinePaint.color = color
-                seg.outlinePaint.strokeCap = Paint.Cap.ROUND
-                seg
-            }
-        }
-
-        withContext(Dispatchers.Main) {
-            map.overlayManager.addAll(segments)
-            map.visibility = View.VISIBLE
-            loadingCourseCircle.visibility = View.INVISIBLE
+            setOnTouchListener(getCustomTouchListener())
         }
     }
 
-
-    private suspend fun drawSlopeMap(geoPoints: ArrayList<AdvancedGeoPoint>, map: MapView, loadingCourseCircle: ProgressBar) {
-        Log.d("DEBUG", "drawSlopeMap started")
-        map.overlayManager.removeAll(map.overlays)
-
-        map.visibility = View.INVISIBLE
-        loadingCourseCircle.visibility = View.VISIBLE
-
-        // Create two color interpolators for uphill and downhill
-        val uphillSlopeColorInterpolator = ArgbEvaluator()
-        val downhillSlopeColorInterpolator = ArgbEvaluator()
-
-        // The size of the window used for moving average
-        val windowSize = 10
-
-        val slopes = geoPoints.windowed(2, 1) { (start, end) ->
-            val distance = end.distanceToAsDouble(start)
-            val altitude = end.altitude - start.altitude
-            (altitude / distance) / 0.13  //13% slope is the darkest color
-        }
-
-        // Apply moving average to the slopes
-        val smoothedSlopes = slopes.windowed(windowSize, 1, partialWindows = true) { it.average() }
-
-        val segments = withContext(Dispatchers.IO) {
-            geoPoints.zip(smoothedSlopes).windowed(2, 1) { (pointWithSlope1, pointWithSlope2) ->
-                val (point1, slope1) = pointWithSlope1
-                val (point2, _) = pointWithSlope2
-
-                val seg = Polyline()
-
-                seg.addPoint(point1)
-                seg.addPoint(point2)
-
-                val color = when {
-                    slope1 > 0 -> {  // Uphill
-                        val slopeRatio = slope1.coerceIn(0.0, 1.0).toFloat()
-                        uphillSlopeColorInterpolator.evaluate(slopeRatio, Color.GREEN, Color.RED) as Int
-                    }
-                    else -> {  // Downhill
-                        val slopeRatio = (-slope1).coerceIn(0.0, 1.0).toFloat()
-                        downhillSlopeColorInterpolator.evaluate(slopeRatio, Color.GREEN, Color.BLUE) as Int
-                    }
+    private fun getCustomTouchListener(): View.OnTouchListener {
+        return View.OnTouchListener { v, event ->
+            val viewPager = binding.scrollviewCourseViewer
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    viewPager.requestDisallowInterceptTouchEvent(true)
                 }
-
-                seg.outlinePaint.color = color
-                seg.outlinePaint.strokeCap = Paint.Cap.ROUND
-                seg
+                MotionEvent.ACTION_UP -> {
+                    viewPager.requestDisallowInterceptTouchEvent(false)
+                    v.performClick()
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    viewPager.requestDisallowInterceptTouchEvent(false)
+                }
             }
+            false
         }
+    }
 
-        withContext(Dispatchers.Main) {
-            map.overlayManager.addAll(segments)
-            map.refreshDrawableState()
-            loadingCourseCircle.visibility = View.INVISIBLE
-            map.visibility = View.VISIBLE
+    private fun loadSegmentsToMap(segments: List<Polyline>) {
+        binding.idMapGpxViewer.apply {
+            visibility = View.INVISIBLE
+            binding.loadingCourseCircle.visibility = View.VISIBLE
+
+            overlayManager.apply {
+                removeAll(overlays)
+                addAll(segments)
+            }
+
+            visibility = View.VISIBLE
+            binding.loadingCourseCircle.visibility = View.INVISIBLE
         }
     }
 
---
+++ app/src/main/java/com/rondinella/strhack/ui/viewer/CourseViewerViewModel.kt
@@ -0,0 +1,153 @@
+package com.rondinella.strhack.ui.viewer
+
+import android.animation.ArgbEvaluator
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rondinella.strhack.tracker.Course
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.osmdroid.views.overlay.Polyline
+import java.io.*
+
+class CourseViewerViewModel : ViewModel() {
+    // LiveData for Course
+    private val _course = MutableLiveData<Course>()
+    val course: LiveData<Course> = _course
+
+    // LiveData for segments
+    private val _segments = MutableLiveData<List<Polyline>>()
+    val segments: LiveData<List<Polyline>> = _segments
+
+    fun loadCourse(file: File) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val course = Course(file)
+            _course.postValue(course)
+        }
+    }
+
+    fun drawBlankMap() {
+        Log.d("DEBUG", "drawBlankMap started")
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val segments = course.value?.geoPoints()?.windowed(2, 1)?.map { (start, end) ->
+                val seg = Polyline().apply {
+                    addPoint(start)
+                    addPoint(end)
+                    outlinePaint.strokeCap = Paint.Cap.ROUND
+                }
+                seg
+            } ?: emptyList()
+
+            withContext(Dispatchers.Main) {
+                _segments.postValue(segments)
+            }
+        }
+    }
+
+    fun drawAltitudeDifferenceMap() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val course = _course.value ?: return@launch
+            val maxAltitude = course.geoPoints().maxByOrNull { it.altitude }?.altitude ?: 0.0
+            val minAltitude = course.geoPoints().minByOrNull { it.altitude }?.altitude ?: 0.0
+
+            val colorInterpolator = ArgbEvaluator()
+
+            val segments = course.geoPoints().windowed(2, 1).mapNotNull { (start, end) ->
+                val altitudeRatio = ((end.altitude - minAltitude) / (maxAltitude - minAltitude)).toFloat().coerceIn(0f, 1f)
+                val color = colorInterpolator.evaluate(altitudeRatio, Color.GREEN, Color.BLACK) as Int
+
+                Polyline().apply {
+                    addPoint(start)
+                    addPoint(end)
+                    outlinePaint.color = color
+                    outlinePaint.strokeCap = Paint.Cap.ROUND
+                }
+            }
+
+            _segments.postValue(segments)
+        }
+    }
+
+    fun drawSlopeMap() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val course = _course.value ?: return@launch
+            val uphillColorInterpolator = ArgbEvaluator()
+            val downhillColorInterpolator = ArgbEvaluator()
+
+            val slopes = course.geoPoints().windowed(2, 1).map { (start, end) ->
+                val distance = end.distanceToAsDouble(start)
+                val altitude = end.altitude - start.altitude
+                altitude / distance / 0.13
+            }
+
+            val smoothedSlopes = slopes.windowed(10, 1, true) { it.average() }
+
+            val segments = course.geoPoints().zip(smoothedSlopes).windowed(2, 1).mapNotNull { (pointWithSlope1, pointWithSlope2) ->
+                val (point1, slope1) = pointWithSlope1
+                val (point2, _) = pointWithSlope2
+
+                val color = when {
+                    slope1 > 0 -> {  // Uphill
+                        val slopeRatio = slope1.coerceIn(0.0, 1.0).toFloat()
+                        uphillColorInterpolator.evaluate(slopeRatio, Color.GREEN, Color.RED) as Int
+                    }
+                    else -> {  // Downhill
+                        val slopeRatio = (-slope1).coerceIn(0.0, 1.0).toFloat()
+                        downhillColorInterpolator.evaluate(slopeRatio, Color.GREEN, Color.BLUE) as Int
+                    }
+                }
+
+                Polyline().apply {
+                    addPoint(point1)
+                    addPoint(point2)
+                    outlinePaint.color = color
+                    outlinePaint.strokeCap = Paint.Cap.ROUND
+                }
+            }
+
+            _segments.postValue(segments)
+        }
+    }
+
+    fun drawSpeedMap() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val course = _course.value ?: return@launch
+            val speedColorInterpolator = ArgbEvaluator()
+
+            val speeds = course.geoPoints().windowed(2, 1).map { (start, end) ->
+                val distance = end.distanceToAsDouble(start)
+                val time = (end.date.time - start.date.time) / 1000
+                distance / time  // Assuming time is in seconds and distance in meters
+            }
+
+            val smoothedSpeeds = speeds.windowed(10, 1, true) { it.average() }
+
+            val maxSpeed = smoothedSpeeds.max() ?: 1.0
+            val minSpeed = smoothedSpeeds.minOrNull() ?: 0.0
+
+            val segments = course.geoPoints().zip(smoothedSpeeds).windowed(2, 1).map { (pointWithSpeed1, pointWithSpeed2) ->
+                val (point1, speed1) = pointWithSpeed1
+                val (point2, _) = pointWithSpeed2
+
+                val speedRatio = ((speed1 - minSpeed) / (maxSpeed - minSpeed)).toFloat().coerceIn(0f, 1f)
+                val color = speedColorInterpolator.evaluate(speedRatio, Color.GREEN, Color.BLACK) as Int
+
+                Polyline().apply {
+                    addPoint(point1)
+                    addPoint(point2)
+                    outlinePaint.color = color
+                    outlinePaint.strokeCap = Paint.Cap.ROUND
+                }
+            }
+
+            _segments.postValue(segments)
+        }
+    }
+
+}
